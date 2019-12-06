ac00f137589922916522d590f89f72f60bc77a85
Dan Lubbers <lubbers.dan@gmail.com>
2019-12-06 13:51:20 -0500

    added aria-labels to social media icon links for accessibility

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -5,19 +5,20 @@ const SocialMedia = () => (
     <div className='social-media-container-nested'>
       <div className='social-media'>
           <div className='github'>
-              <a href='http://github.com/danlubbers' target='_blank' rel="noopener noreferrer" alt='github'><FaGithub/>
+              <a href='http://github.com/danlubbers' aria-label='github danlubbers' target='_blank' rel="noopener noreferrer" alt='github'><FaGithub/>
               </a>
           </div>
           <div className='linkedin'>
-            <a href='http://linkedin.com/in/danlubbers' target='_blank' rel="noopener noreferrer" alt='linkedin'><FaLinkedin />
+            <a href='http://linkedin.com/in/danlubbers' aria-label='inkedin danlubbers'
+            target='_blank' rel="noopener noreferrer" alt='linkedin'><FaLinkedin />
             </a>
           </div>
           <div className='instagram'>
-              <a href='http://instagram.com/danlubbers' target='_blank' rel="noopener noreferrer" alt='instagram'><FaInstagram />
+              <a href='http://instagram.com/danlubbers' aria-label='instagram danlubber' target='_blank' rel="noopener noreferrer" alt='instagram'><FaInstagram />
               </a>
           </div>
           <div className='camera'>
-              <a href='http://danlubbersphotographs.com' target='_blank' rel="noopener noreferrer" alt='dan lubbers photographs'><FaCamera />
+              <a href='http://danlubbersphotographs.com' aria-label='dan lubbers photographs' target='_blank' rel="noopener noreferrer" alt='dan lubbers photographs'><FaCamera />
               </a>
           </div>
       </div>
