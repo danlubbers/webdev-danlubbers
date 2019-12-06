f99a0e62378de871bdb73453ed86204958d2494e
Dan Lubbers <lubbers.dan@gmail.com>
2019-12-06 14:20:27 -0500

    react helmet seo was not working, hopefully fixed

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,20 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
 import './index.scss'
 
 
 const Layout = ({ children, data }) => (
   <div>
-    <Helmet
-      title='Dan Lubbers | Full Stack Web Developer'
-    
-      meta={[
-        {author: 'Dan Lubbers'},
-        { name: 'description', content: 'Dan Lubbers is a Photographer / Retoucher / Web Developer specializing in Studio and Environmental Portraits in Saratoga Springs, NY.' },
-        { name: 'keywords', content: 'dan lubbers, web, web developer, developer, development, full stack, coder, javascript, react, reactjs, programmer, photographer, photography, photographs, saratoga, springs, ny, new york, louisville, kentucky, ky, fine art, fine art portraiture, editorial, portraits, portraiture, environmental portraits, studio, product, people, emotion, passion, adventure, action, lifestyle, scenic, landscape, iphoneography, music, musicians, will, oldham, jim, james, my morning jacket, mmj, bonnie, prince, billy, sport, sports, red river gorge, rock climbing, climbing, climber, rockclimbing, headshots, commercial, cycling, trailrunning, running, athletes, professional, athletic, local' },
-      ]}
-    />
+
     <div
       style={{
         margin: '0 auto',
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,17 @@ import Skillset from '../components/Skillset'
 import Projects from '../components/Projects'
 import Header from '../components/Header'
 import '../layouts/index.scss';
+import Helmet from 'react-helmet';
 
 const IndexPage = () => (
     <div className='app-wrapper'>
+        <Helmet>
+            <html lang="en" />
+            <title>Dan Lubbers | Full Stack Web Developer</title>
+            <meta author='Dan Lubbers' />
+            <meta name='description' content='Dan Lubbers is a Photographer / Retoucher / Web Developer specializing in Studio and Environmental Portraits in Saratoga Springs, NY.' />
+            <meta name='keywords' content='dan lubbers, web, web developer, developer, development, full stack, coder, javascript, react, reactjs, programmer, photographer, photography, photographs, saratoga, springs, ny, new york, louisville, kentucky, ky, fine art, fine art portraiture, editorial, portraits, portraiture, environmental portraits, studio, product, people, emotion, passion, adventure, action, lifestyle, scenic, landscape, iphoneography, music, musicians, will, oldham, jim, james, my morning jacket, mmj, bonnie, prince, billy, sport, sports, red river gorge, rock climbing, climbing, climber, rockclimbing, headshots, commercial, cycling, trailrunning, running, athletes, professional, athletic, local' />
+        </Helmet>
         <div>
           <Header />
             <Bio /> 
